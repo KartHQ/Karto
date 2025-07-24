b25a72220e9f07e7a5a37ba75847c1be114adff4
Ain Grace <qnicat32@gmail.com>
2025-07-24 20:34:50 +0400

    partially implement gathering of UserActivitySnapshot from db

--- src/main/java/org/project/karto/domain/card/repositories/GiftCardRepository.java
+++ src/main/java/org/project/karto/domain/card/repositories/GiftCardRepository.java
@@ -1,13 +1,16 @@
 package org.project.karto.domain.card.repositories;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.project.karto.domain.card.entities.GiftCard;
 import org.project.karto.domain.card.value_objects.BuyerID;
 import org.project.karto.domain.card.value_objects.CardID;
 import org.project.karto.domain.card.value_objects.OwnerID;
 import org.project.karto.domain.card.value_objects.StoreID;
+import org.project.karto.domain.card.value_objects.UserActivitySnapshot;
 import org.project.karto.domain.common.containers.Result;
 
-import java.util.List;
 
 public interface GiftCardRepository {
 
@@ -22,4 +25,6 @@ public interface GiftCardRepository {
     Result<List<GiftCard>, Throwable> findBy(OwnerID ownerID);
 
     Result<List<GiftCard>, Throwable> findBy(StoreID storeID);
+
+    Result<UserActivitySnapshot, Throwable> findBy(UUID userID);
 }
